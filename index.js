e8061b9a0795cd68de236315ca8d5ea08ed44746
Jatin Hazrati <jatinhazrati@gmail.com>
2020-04-03 23:29:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-app = require('./app')
-const port = process.env.PORT || 3000
+const mongoose = require("mongoose");
 
-app.listen(port, () => {
-    console.log("Server is up on the port" + port)
-})
+// import environmental variables from our variables.env file
+require("dotenv").config({ path: "variables.env" });
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on("error", err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// READY?! Let's go!
+
+// import all of our models
+require("./models/Story");
+require("./models/User");
+require("./models/Review");
+
+// Start our app!
+const app = require("./app");
+app.set("port", process.env.PORT || 7777);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
